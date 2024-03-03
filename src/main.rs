0ec00fd768910dbe8cc7a4b0fa0f45efb437d7b7
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2024-03-03 16:19:43 -0500

    chore: try to do handle user input

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
-use std::io;
+use crossterm::{
+    cursor,
+    event::{self, read, Event, KeyCode, KeyEvent},
+    execute,
+    terminal::{self, Clear, ClearType},
+};
 use std::str::FromStr;
 use std::{collections::HashMap, io::Read};
+use std::{
+    hash::Hash,
+    io::{self, Stdout, Write},
+};
 
 fn main() {
     // let action = std::env::args().nth(1).expect("Please specify an action");
@@ -8,66 +17,114 @@ fn main() {
 
     let mut todo = Todo::new().expect("> Error to initialize the DB.");
 
-    loop {
-        println!("Enter action (add, complete, delete) and item (or 'quit' to exit):");
-
-        let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read line");
-
-        let trimmed_input = input.trim().to_lowercase();
-        let mut parts = trimmed_input.split_whitespace();
-
-        let action = match parts.next() {
-            Some(a) => a,
-            None => {
-                println!("Invalid input. Please try again");
-                continue;
-            }
-        };
-
-        if action == "quit" {
-            break;
-        }
-
-        let item = match parts.next() {
-            Some(i) => i,
-            None => {
-                println!("Please specify an item");
-                continue;
-            }
-        };
-
-        match action {
-            "add" => {
-                todo.insert(item.to_string());
-                match todo.save() {
-                    Ok(_) => println!("> Todo saved"),
-                    Err(why) => println!(">An error occurred: {}", why),
-                }
-            }
-            "complete" => match todo.complete(&item.to_string()) {
-                None => println!("> '{}' is not present in the list", item),
-                Some(_) => match todo.save() {
-                    Ok(_) => println!("> Todo saved"),
-                    Err(why) => println!("> An error ocurred {}", why),
-                },
-            },
-            "delete" => {
-                todo.delete(&item.to_string());
-                match todo.save() {
-                    Ok(_) => println!("> Todo saved"),
-                    Err(why) => println!("> An error has occurred: {}", why),
-                }
-            }
-            _ => {
-                println!("> Invalid action. Valid action are 'add', 'complete' and 'delete'")
-            }
-        }
+    // setup terminal
+    let mut stdout = io::stdout();
+    match terminal::enable_raw_mode() {
+        Err(why) => eprintln!(
+            "> An error occurred when trying to enable raw mode: {}",
+            why
+        ),
+        Ok(_) => (),
     }
 
-    println!("> Exiting todo list application");
+    match execute!(stdout, cursor::Hide) {
+        Err(why) => eprintln!(
+            "> An error occurred when trying to hide the cursor: {}",
+            why
+        ),
+        Ok(_) => (),
+    }
+
+    let mut selected_index = 0;
+
+    loop {
+        // draw ui
+        match draw_ui(&mut stdout, &todo.map, selected_index) {
+            Err(why) => eprintln!("> Error to draw ui cli: {}", why),
+            Ok(_) => (),
+        }
+
+        // handle user input
+        if let Ok(event) = read() {
+            match event {
+                Event::Key(key_event) => match key_event.code {
+                    KeyCode::Char(' ') => {
+                        todo.toggle_by_index(selected_index);
+                        match todo.save() {
+                            Ok(_) => {}
+                            Err(why) => eprintln!("> Error to save todo: {}", why),
+                        }
+                    }
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        break;
+                    }
+                    _ => {}
+                },
+                _ => {}
+            }
+        } else {
+            eprintln!("> Error to read the user input");
+        }
+
+        println!("Enter action (add, complete, delete) and item (or 'quit' to exit):");
+
+        // let mut input = String::new();
+        // io::stdin()
+        //     .read_line(&mut input)
+        //     .expect("Failed to read line");
+
+        // let trimmed_input = input.trim().to_lowercase();
+        // let mut parts = trimmed_input.split_whitespace();
+
+        // let action = match parts.next() {
+        //     Some(a) => a,
+        //     None => {
+        //         println!("Invalid input. Please try again");
+        //         continue;
+        //     }
+        // };
+
+        // if action == "quit" {
+        //     break;
+        // }
+
+        // let item = match parts.next() {
+        //     Some(i) => i,
+        //     None => {
+        //         println!("Please specify an item");
+        //         continue;
+        //     }
+        // };
+
+        // match action {
+        //     "add" => {
+        //         todo.insert(item.to_string());
+        //         match todo.save() {
+        //             Ok(_) => println!("> Todo saved"),
+        //             Err(why) => println!(">An error occurred: {}", why),
+        //         }
+        //     }
+        //     "complete" => match todo.complete(&item.to_string()) {
+        //         None => println!("> '{}' is not present in the list", item),
+        //         Some(_) => match todo.save() {
+        //             Ok(_) => println!("> Todo saved"),
+        //             Err(why) => println!("> An error ocurred {}", why),
+        //         },
+        //     },
+        //     "delete" => {
+        //         todo.delete(&item.to_string());
+        //         match todo.save() {
+        //             Ok(_) => println!("> Todo saved"),
+        //             Err(why) => println!("> An error has occurred: {}", why),
+        //         }
+        //     }
+        //     _ => {
+        //         println!("> Invalid action. Valid action are 'add', 'complete' and 'delete'")
+        //     }
+        // }
+    }
+
+    // println!("> Exiting todo list application");
 
     // if action == "add" {
     //     todo.insert(item);
@@ -131,12 +188,87 @@ impl Todo {
 
     fn complete(&mut self, key: &String) -> Option<()> {
         match self.map.get_mut(key) {
-            Some(v) => Some(*v = false),
+            Some(v) => Some(*v = true),
             None => None,
         }
+    }
+
+    fn toggle_by_index(&mut self, index: usize) -> Option<()> {
+        // let key = self.map.keys().nth(index).expect("Invalid index");
+        // match self.map.get_mut(key) {
+        //     Some(v) => Some(*v = !*v),
+        //     None => None,
+        // }
+        println!("Toggle todo {}", index);
+        if let Some(key) = self.map.keys().nth(index) {
+            if let Some(value) = self.map.get_mut(&key.to_owned()) {
+                *value = !*value;
+                return Some(());
+            }
+        }
+        None
     }
 
     fn delete(&mut self, key: &String) {
         self.map.remove(key);
     }
+}
+
+enum Direction {
+    Up,
+    Down,
+}
+
+fn move_cursor(todos: &HashMap<String, bool>, current_index: usize, direction: Direction) -> usize {
+    match direction {
+        Direction::Up => {
+            if current_index > 0 {
+                current_index - 1
+            } else {
+                current_index
+            }
+        }
+        Direction::Down => {
+            if current_index < todos.len() - 1 {
+                current_index + 1
+            } else {
+                current_index
+            }
+        }
+    }
+}
+
+fn draw_ui(
+    stdout: &mut Stdout,
+    todos: &HashMap<String, bool>,
+    selected_index: usize,
+) -> Result<(), io::Error> {
+    execute!(
+        stdout,
+        crossterm::terminal::Clear(ClearType::All),
+        crossterm::cursor::MoveTo(0, 0),
+        crossterm::style::Print("Todo List\n"),
+    )?;
+    let mut i = 0;
+
+    for (key, value) in todos {
+        let checkbox = if i == selected_index { "(*)" } else { "( )" };
+        let color = if *value {
+            crossterm::style::Color::Green
+        } else {
+            crossterm::style::Color::White
+        };
+
+        crossterm::execute!(
+            stdout,
+            crossterm::style::SetForegroundColor(color),
+            crossterm::style::Print(format!("{} {}\n", checkbox, key)),
+            crossterm::style::ResetColor,
+        )?;
+
+        i += 1;
+    }
+
+    stdout.flush()?;
+    Ok(())
 }
